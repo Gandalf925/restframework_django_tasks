858f920f6a1a43ab97ce1e11ef37557ffdbe414c
gandalf925 <theengineerofoz@gmail.com>
2021-02-28 11:43:30 +0900

    Rest API created (checked)

--- api/models.py
+++ api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Task(models.Model):
-  title = models.CharField(max_length=50,)
+  title = models.CharField(max_length=50)
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
 
